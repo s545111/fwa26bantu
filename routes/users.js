cb16d7ad896666658827015e36337759f3b59521
varunbantu <89408502+varunbantu@users.noreply.github.com>
2021-10-27 15:48:55 -0500

    changes made to users.js page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+value1 = 0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  value1 = value1 +1;
+  res.send('User accesses are: '+value1);
 });
 
 module.exports = router;
